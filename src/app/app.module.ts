415424c25cb83f04d34f6d076ce58c4e3ea636b4
Milinda Rajapakshe <milinda@codegen.net>
2020-03-26 21:25:22 +0530

    red, blue, green components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import { ServerComponent } from './server/server.component';
+import { RedComponent } from './colour/red/red.component';
+import { BlueComponent } from './colour/blue/blue.component';
+import { GreenComponent } from './colour/green/green.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    RedComponent,
+    BlueComponent,
+    GreenComponent
   ],
   imports: [
     BrowserModule,
